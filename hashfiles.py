1fcf903de5a9b1d453c26cdc92f23fc6b9413be5
Ken Elliott <Kennethpelliott@outlook.com>
2023-02-24 05:08:25 -0500

    removed blank lines

--- hashfiles.py
+++ hashfiles.py
@@ -1,33 +1,24 @@
 import os
 import hashlib
-
-
 def gethashes(path):
     filesANDfolders = os.listdir(path)
-
     filesHashList = list()
-
     files = [f for f in filesANDfolders if os.path.isfile(path + '/' + f)]  # Filtering only the files.
     fullFilePath = list()
     for x in files:
         fullFilePath.append(path +x)
         file = path  + x # Location of the file (can be set a different way)
         block_size = 65536 # The size of each read from the file
-
         file_hash = hashlib.sha256() # Create the hash object, can use something other than `.sha256()` if you wish
         with open(file, 'rb') as f: # Open the file to read its bytes
             fb = f.read(block_size) # Read from the file. Take in the amount declared above
             while len(fb) > 0: # While there is still data being read from the file
                 file_hash.update(fb) # Update the hash
                 fb = f.read(block_size) # Read the next block from the file
-
         filesHashList.append(file_hash.hexdigest())
-
     file_dic = {}
-
     keys = fullFilePath
     values = filesHashList
-
     for i in range(len(values)):
         file_dic[keys[i]] = values[i]
     return file_dic
--- setjson.py
+++ setjson.py
@@ -1,26 +1,18 @@
 import hashfiles
 import os
 import json
-
 filedic = dict()
-
 # walk through directory and all subdirectories
-
 print("\033[95mEnter a directory below to create a \033[96m'files.json'\033[0m\033[95m file to save the state of the "
       "directory :\n ")
-
 directory = input()
-
 subdirs = [x[0] for x in os.walk(directory)]
-
 for x in subdirs:
     filedic = {**filedic, **hashfiles.gethashes(x + "/")}
-
 print("\033[96m\n'files.json'\033[0m file created or overwitten in \033[92m'" + directory + "'\033[0m\n\n\033["
                                                                                             "94mBelow is what it "
                                                                                             "looks ""like:\n\033[0m")
 print("{" + "\n".join("{!r}: {!r},".format(k, v) for k, v in filedic.items()) + "}")
-
 json = json.dumps(filedic)
 f = open(directory + "/files.json","w")
 f.write(json)
