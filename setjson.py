7ca869d2a93c373b97f87e85080baec1713a862f
Ken Elliott <Kennethpelliott@outlook.com>
2023-02-25 17:36:00 -0500

    grammer

--- setjson.py
+++ setjson.py
@@ -3,13 +3,13 @@ import os
 import json
 filedic = dict()
 # walk through directory and all subdirectories
-print("\033[95mEnter a directory below to create a \033[96m'files.json'\033[0m\033[95m file to save the state of the "
-      "directory :\n\033[0m\n ")
+print("\033[95mEnter a directory below where a \033[96m'files.json'\033[0m\033[95m file will be saved that will store"
+      "the state of the directory :\n\033[0m\n ")
 directory = input()
 subdirs = [x[0] for x in os.walk(directory)]
 for x in subdirs:
     filedic = {**filedic, **hashfiles.gethashes(x + "/")}
-print("\033[96m\n'files.json'\033[0m file created or overwitten in \033[92m'" + directory + "'\033[0m\n\n\033["
+print("\033[96m\n'files.json'\033[0m file created or overwritten in \033[92m'" + directory + "'\033[0m\n\n\033["
                                                                                             "94mBelow is what it "
                                                                                             "looks ""like:\n\033[0m")
 print("{" + "\n".join("{!r}: {!r},".format(k, v) for k, v in filedic.items()) + "}")
